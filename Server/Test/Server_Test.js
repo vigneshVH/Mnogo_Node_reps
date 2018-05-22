b36c161c8643f17da7ad3c56421310b996dc7898
Vignesh P <EVOLVUS\vigneshp@EPCBLR-052.evolvus.com>
2018-05-22 12:50:41 +0530

    GET/todos

--- Server/Test/Server_Test.js
+++ Server/Test/Server_Test.js
@@ -4,10 +4,18 @@ const request=require('supertest');
 const {app}=require('./../Server');
 const {Todo}=require('./../Models/Todo');
 
+const todos=[{
+  text:'First test  todo'
+},{
+  text:'Second todo'
+
+}];
+
+
 beforeEach((done)=>{
   Todo.remove({}).then( () => {
-    done();
-});
+    return Todo.insertMany(todos);
+}).then(()=>done());
 });
 
 
@@ -32,7 +40,7 @@ describe('POST /todos',()=>
                           return done(err);
                                 }
 
-                                Todo.find().then((todos)=>{
+                                Todo.find({text}).then((todos)=>{
                                   expect(todos.length).toBe(1);
                                     expect(todos[0].text).toBe(text);
                                     done();
@@ -56,9 +64,22 @@ describe('POST /todos',()=>
 
                           Todo.find().then((todos)=>{
                             console.log(todos.length);
-                            expect(todos.length).toBe(0);
+                            expect(todos.length).toBe(2);
                             done();
                           }).catch((e)=> done(e));
                       });
       });
   });
+
+  describe('GET/todos',()=>
+{
+  it('should get all todos',(done)=>
+{
+  request(app)
+  .get('/todos')
+  .expect(200)
+  .expect((res)=>{
+    expect(res.body.todos.length).toBe(2);
+  }).end(done);
+});
+});
