894894dff898751552b0a34e09e217598c8b1e37
Vignesh P <EVOLVUS\vigneshp@EPCBLR-052.evolvus.com>
2018-05-24 11:36:41 +0530

    POST invoove in postman

--- Server/Server.js
+++ Server/Server.js
@@ -23,9 +23,15 @@ app.post('/todos',(req,res)=>{
 {
 res.status(400).send(e);
 });
-
-
 });
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  })
+})
 
 
 app.listen(3000,()=>
