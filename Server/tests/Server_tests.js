428ae59effa716d3721845899a262934c51d9c45
Vignesh P <EVOLVUS\vigneshp@EPCBLR-052.evolvus.com>
2018-05-24 15:52:28 +0530

    extra 2 test case

--- Server/tests/Server_tests.js
+++ Server/tests/Server_tests.js
@@ -1,10 +1,16 @@
 const expect=require('expect');
 const request=require('supertest');
-
+const {ObjectId}=require('mongodb');
 const {app}=require('./../Server');
 const {Todo}=require('./../Models/todo');
 
-const todos=[{text:"First test todo"},{text:"Second test todo"}];
+const todos=[{
+  _id:new ObjectId(),
+  text:"First test todo"
+},{
+  _id:new ObjectId(),
+  text:"Second test todo"
+}];
 
 
 beforeEach((done)=>
@@ -85,4 +91,39 @@ describe('GET/todos',()=>
   })
   .end(done);
 });
+});
+
+
+describe('GET/todos/:id',()=>
+{
+  it('Should return todo doc',(done)=>
+{
+  request(app)
+  .get(`/todos/${todos[0]._id.toHexString()}`)
+  .expect(200)
+  .expect((res)=>
+  {
+  expect(res.body.todo.text).toBe(todos[0].text);
+  })
+      .end(done);
+    });
+
+it('should return 404 if todo not found',(done)=>
+{
+  var hexId=new ObjectId().toHexString();
+
+  request(app)
+  .get(`/todos/${hexId}`)
+  .expect(404)
+  .end(done);
+});
+it('should return 404 if non obj ids',(done)=>
+{
+
+  request(app)
+  .get('/todos/123abc')
+  .expect(404)
+  .end(done);
+});
+
 });
