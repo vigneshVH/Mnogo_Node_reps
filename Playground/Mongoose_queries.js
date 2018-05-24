492dd0899b43dd970ca81baa460622bb135a46d9
Vignesh P <EVOLVUS\vigneshp@EPCBLR-052.evolvus.com>
2018-05-24 12:58:11 +0530

    User working now :)

--- Playground/Mongoose_queries.js
+++ Playground/Mongoose_queries.js
@@ -1,45 +1,47 @@
-const {mongoose}=require('./../Server/Db/mongoose');
-const {Todo}=require('./../Server/Models/Todo');
-const {ObjectID}=require('mongodb');
-const {Vinai}=require('./../Server/Models/Vinai');
-//var id="5b0254fa55f8cc6f10d0db62";
+const {ObjectId}=require('mongodb');
+const {mongoose}=require('./../Server/DB/mongoose');
+const {Todo}=require('./../Server/Models/todo');
+const {User}=require('./../Server/Models/user');
 
+var id='52b065c6da9f9a5076a72714a';
 
-// if(!ObjectID.isValid(id))
-// {
-//   console.log("it is a not a valid id");
-// }
-// Todo.find({
-//   _id:id
-// }).then((todos)=>
-// {
-//   console.log('Todos',todos);
-// });
-//
-//
-// Todo.findOne({
-//   _id:id
-// }).then((todo)=>
-// {
-//   console.log('Todo',todo);
-// });
-
-// Todo.findById(id).then((todo)=>
-// {
-//   if(!todo)
-//   {
-//   return console.log("ID not found");
-//   }
-//   console.log('Todo by id',todo);
-// }).catch((e)=> console.log(e));
-Vinai.findById("5b03c43b77f5240a407b10b0").then((doc)=>
+if(!ObjectId.isValid(id))
 {
-  if(!doc)
-  {
-    return console.log('Unable to  find Vinai');
-  }
-  console.log(JSON.stringify(doc,undefined,2));
-},(e)=>{
-  console.log(e);
+  console.log("ID not valid");
 }
-);
+
+
+
+
+Todo.find({
+  _id:id
+}).then((doc1)=>{
+  console.log("todos",doc1);
+});
+
+Todo.findOne({
+  _id:id
+}).then((doc2)=>
+{
+  console.log('Todo',doc2);
+});
+
+Todo.findById(id).then((doc3)=>
+{
+  if(!doc3)
+  {
+    return console.log("ID not valid");
+  }
+  console.log("Find by id",doc3);
+}).catch((e)=> console.log(e));
+
+
+
+User.findById(id).then((user)=>
+{
+ if(!user)
+ {
+   return console.log("Unable to find user");
+ }
+ console.log("vinai" ,user);
+}).catch((e)=> console.log(e));
